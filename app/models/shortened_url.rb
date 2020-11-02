de8afbd3fe22dc46edf0c65ebf44bada639cb47a
Valerie Campbell <valeriecs10@gmail.com>
2020-11-02 15:12:01 -0800

    add associations to and through Visit, add "num"- methods to ShortenedUrl

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -7,6 +7,16 @@ class ShortenedUrl < ApplicationRecord
     foreign_key: :user_id,
     primary_key: :id
 
+  has_many :visits,
+    class_name: :Visit,
+    foreign_key: :shortened_url_id,
+    primary_key: :id
+
+  has_many :visitors,
+    -> { distinct }, 
+    through: :visits, 
+    source: :visitor
+
   def self.create_for_user_and_long_url(user, long_url)
     raise 'User must be in database' unless User.exists?(:id => user.id)
     raise 'Long url must be a string' unless long_url.is_a?(String)
@@ -25,5 +35,19 @@ class ShortenedUrl < ApplicationRecord
     code
   end
 
-  self
+  def num_clicks
+    visits.count
+  end 
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits
+      .select(:user_id)
+      .where('created_at >= ?', 10.minutes.ago)
+      .distinct
+      .count
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
     foreign_key: :user_id,
     primary_key: :id
 
+  has_many :visits,
+    class_name: :Visit,
+    foreign_key: :user_id,
+    primary_key: :id
+
+  has_many :visited_urls, 
+    -> { distinct },
+    through: :visits, 
+    source: :visited_url
+
 end
