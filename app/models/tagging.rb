cb7f191b0079f92b57c5efb0176e7217c8117f8b
Valerie Campbell <valeriecs10@gmail.com>
2020-11-04 15:01:12 -0800

    add uniqueness validation for topic/url pairs

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,3 +1,7 @@
 class Tagging < ApplicationRecord
   validates :tag_topic_id, :shortened_url_id, presence: true
+  validates :tag_topic_id, uniqueness: { 
+    scope: :shortened_url_id,
+    message: 'url can only have one tag per topic'
+  }
 end
